7f32bd926ef598035b3faa9c681bdbb81cc3e7e1
shengjie315 <553502618@qq.com>
2022-05-27 17:44:18 +0800

    gradle配置

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# standrad
+# standard 组件+集成式开发项目搭建
+common 标准类库，包括常用第三方框架的封装
+
---
+++ login/src/main/java/com/shengj/login/Login_MainActivity.java
@@ -0,0 +1,14 @@
+package com.shengj.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity_main);
+    }
+}
