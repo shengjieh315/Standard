55c2f885142ad20ad8539d57884ecf58818c3df4
shengjie315 <553502618@qq.com>
2022-05-28 13:48:42 +0800

    gradle动态隔离

---
+++ login/src/main/java/com/shengj/login/debug/Login_DebugMainActivity.java
@@ -0,0 +1,16 @@
+package com.shengj.login.debug;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.shengj.login.R;
+
+public class Login_DebugMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity_main);
+    }
+}
